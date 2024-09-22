353d1aa643a547e650b7aaf90b967b9ad3ab4ed6
Ian Cullen <ian.james.cullen@gmail.com>
2024-09-22 01:04:19 +0100

    Format on a single line for readability

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,57 +12,13 @@ return {
 			},
 		},
 		keys = {
-			{
-				"<leader>ff",
-				function()
-					require("telescope.builtin").find_files()
-				end,
-				desc = " Telescope [f]ind [f]iles",
-			},
-			{
-				"<leader>gf",
-				function()
-					require("telescope.builtin").git_files()
-				end,
-				desc = "Telescope [g]it [f]iles",
-			},
-			{
-				"<leader>pd",
-				function()
-					require("telescope.builtin").diagnostics()
-				end,
-				desc = "Telescope [p]roject [d]iagnostics",
-			},
-			{
-				"<leader>ps",
-				function()
-					require("telescope.builtin").grep_string({
-						search = vim.fn.input("Grep > "),
-					})
-				end,
-				desc = "Telescope [p]roject [s]earch",
-			},
-			{
-				"<leader>sg",
-				function()
-					require("telescope.builtin").live_grep()
-				end,
-				desc = "Telescope [S]earch with [G]rep",
-			},
-			{
-				"<leader>sh",
-				function()
-					require("telescope.builtin").help_tags()
-				end,
-				desc = "Telescope [S]earch [H]elp",
-			},
-			{
-				"<leader>sk",
-				function()
-					require("telescope.builtin").keymaps()
-				end,
-				desc = "Telescope [S]earch [K]eymaps",
-			},
+			{ "<leader>ff", function() require("telescope.builtin").find_files() end, desc = " Telescope [f]ind [f]iles"},
+			{ "<leader>gf", function() require("telescope.builtin").git_files() end, desc = "Telescope [g]it [f]iles"},
+			{ "<leader>pd", function() require("telescope.builtin").diagnostics() end, desc = "Telescope [p]roject [d]iagnostics"},
+			{ "<leader>ps", function() require("telescope.builtin").grep_string({search = vim.fn.input("Grep > ")}) end, desc = "Telescope [p]roject [s]earch"},
+			{ "<leader>sg", function() require("telescope.builtin").live_grep() end, desc = "Telescope [S]earch with [G]rep"},
+			{ "<leader>sh", function() require("telescope.builtin").help_tags() end, desc = "Telescope [S]earch [H]elp"},
+			{ "<leader>sk", function() require("telescope.builtin").keymaps() end, desc = "Telescope [S]earch [K]eymaps"},
 		},
 		cmd = "Telescope",
 		opts = {
